f0328022d7ebd58ac3d387312924945922f64ed9
unknown <david.silva.lee@gmail.com>
2018-06-18 22:40:16 -0600

    some clean up

--- js/sw.js
+++ js/sw.js
@@ -24,14 +24,9 @@ self.addEventListener('install', (event) =>
 self.addEventListener('fetch', (event) => {
   let requestUrl = new URL(event.request.url);
   let cacheReq = event.request;
-  console.log("checking: ", cacheReq, "and ", requestUrl)
   if (requestUrl.pathname.startsWith('/restaurant.html')) {
       cacheReq = new Request('restaurant.html');
   }
-  if (requestUrl.hostname != 'localhost') {
-      event.request.mode = 'no-cors';
-  }
-
   event.respondWith (
       caches.match(cacheReq).then((response) => {
           return (
@@ -41,7 +36,7 @@ self.addEventListener('fetch', (event) => {
                     return fetchResponse;
                 })
             ).catch((error) => {
-                if (event.request.url.pathname.startsWith('.jpg')) {
+                if (event.request.url.pathname.startsWith('/*.jpg')) {
                     return caches.match('/img/');
                 }
                 return new Response('Application in offline mode', {
